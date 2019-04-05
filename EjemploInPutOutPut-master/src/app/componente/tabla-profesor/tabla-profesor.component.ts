fadd9253c3b055e7e1b78aec0db674f37f0d7790
alumno <alumno@utnfra.com.ar>
2019-04-04 21:52:03 -0300

    cambios

---
+++ EjemploInPutOutPut-master/src/app/componente/tabla-profesor/tabla-profesor.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-tabla-profesor',
+  templateUrl: './tabla-profesor.component.html',
+  styleUrls: ['./tabla-profesor.component.css']
+})
+export class TablaProfesorComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ EjemploInPutOutPut-master/src/app/componentes/tabla-profesor/tabla-profesor.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit, Input, Output, EventEmitter } from "@angular/core";
+import { Profesor } from 'src/app/clases/profesor';
+
+@Component({
+  selector: 'app-tabla-profesor',
+  templateUrl: "./tabla-profesor.component.html",
+  styleUrls: ["./tabla-profesor.component.css"]
+})
+export class TablaProfesorComponent implements OnInit {
+  @Input() listadoProfesores: Profesor[];
+  @Output() profesorSeleccionado: EventEmitter<any> = new EventEmitter<any>();
+  constructor() {}
+  ngOnInit() {}
+  mostrarDetalles(parametroProfesor) {
+    this.profesorSeleccionado.emit(parametroProfesor);
+  }
+}
