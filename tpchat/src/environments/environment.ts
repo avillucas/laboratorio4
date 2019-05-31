2a3b13c3478ca8e2624603529e7b29547d1985ad
alumno <alumno@utnfra.com.ar>
2019-05-30 21:57:10 -0300

    cambios

--- tpchat/src/environments/environment.ts
+++ tpchat/src/environments/environment.ts
@@ -3,7 +3,22 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    type: 'service_account',
+    project_id: 'tplaboratorio4-a',
+    private_key_id: '32c5206cb222582eec977955802f90d2adfc4ff6',
+    // tslint:disable-next-line: max-line-length
+    private_key: '-----BEGIN PRIVATE KEY-----\nMIIEuwIBADANBgkqhkiG9w0BAQEFAASCBKUwggShAgEAAoIBAQCEoNsUg5IDBUkD\nuutnuuMWAadgke6jpsTiWyRmjctUzjhYv9+ihm63YVn7qs72fE4BmKVx6jSLh6fD\nRr0ahhuDbrl84ugZ6sc/EIJLf5RSb3PHllyHaBtqADhZ1sGAb5sWWFGU4X6MHGF6\nWPdcsNm9MfeDDOG8ZZT9Wg6QiWHxghP9KvPmF04yq7JpS28OAeTpIBpg+WumeX8Q\nxME7t3Xf/1jFUv8UlSu1rCQJiOdtcla6EwHcMR9zejanfE6/GSB2TEoKvlFcCXkn\neWlNcV7Pqm1qdbRFvywtT7FiLU6xmWyTC/8Gngc06h1CKSmMHgewDWYdGODyLp9h\nIjtVDGAPAgMBAAECgf8sAaiSYHSrzb1dsq8Y2Uj1EvZ92e4LoM3e/P/N1DdEPNDq\nddXFqdy67Xxxlyy9EXpqMb+sTmjXxff6OcBZXR7LQvH/x1jy2h/GFj80AQsCpw5d\nMnhOAH764DxkqLi/ENtWEl5efQXt+yT90qR7g1c/sVvyqs/kEihdcwflyOMVycxU\nG3eXtAhfbUYarsHDx5bkXJ5tNIzZZnen0aMkKZieKAFs0E3nVH1E+KQQqSEvFaGf\nhQD69uBP0WLs6JYAeQIiuza7Di8BOfb+k/DMC/KI/NF7bR7AhhQHxHPXPRe4C9cH\nY1kaBp8RfyW23H+0iHd6p9auMnOQe7e2W86ZXpECgYEAukZGQcq6Rg5OX457ze/3\n87np0NFUYh4iLgadkGYSA0XJqrmdWeSmCMoTW8OhEx97eK7nfouLvg4c4fqaa0fG\na8NECxElEgaYQB9Dz9g3RwOrX/p3cfjkx9p6D1l1Ws5XhOiypme6R6BJvLBlsaye\n2T3kzH6oGDz7EdqZuM1I3qkCgYEAtkXyJD5pnQ1z1GWppMNixZHd81DMi3peXtIu\nUPynccz8Fs9KIvt6DODSqLvpkUOWuXe6dSaMfzffv0ak2Lbd04A87KQOhuFslBJe\n3muI9p0tWNSbDi8ZgQzG6sVdkT/V9oINS8dEVeZrB2oKo+OeshGbX0IGYyFKuiSS\nDXbfE/cCgYBPUz/2IqQi01LS5LWaZJfeBg0pbQuXYSfDG/QbmywOkrin6LW2o2WF\nlrCZwnykzSJiqXFR9ZFEFXR9U6BREVbRa21dnSaBP6e0ntvc9kHMDyb7D4mwUoLz\nDPov8TPFwNuKcSIrNT8h44OjRCd6e32GHBKEd2eUMtvkpYgTJ/DhSQKBgECV7ET3\ncIjb2W0rUL83FGqZUyICQVgy1fmXVB4SSGrEjZavgL+AiDDEASEU5KA6aM/zIric\nHjB95eRq7cJ0mYl16SCYyST1/3lAQSlixTdNXmIZg4+1FL5rfph1sr/i9LfSQU/W\nndoqOUZ5Rqfn4bWLE5jfVlAN3eKzTqVCt6zlAoGBAJVgu4N8wFs7ydJkdiejwlHI\nA7iaBYLAS+CuWcgwoKy7YSORvtfa7E72RpxTzRySt0dWGhzGFMf3twQn7MdG97Ad\n+ZEhw/GItuKfZjyfHV2dbKAKheUGiL6Zu96epsTf5u4Ea/93eF9Wez8ULi3IMcDY\nkagM/h+LT42D2eMPpM6o\n-----END PRIVATE KEY-----\n',
+    client_email: 'firebase-adminsdk-xwc47@tplaboratorio4-a.iam.gserviceaccount.com',
+    client_id: '106143599116226660385',
+    auth_uri: 'https://accounts.google.com/o/oauth2/auth',
+    token_uri: 'https://oauth2.googleapis.com/token',
+    auth_provider_x509_cert_url: 'https://www.googleapis.com/oauth2/v1/certs',
+    // tslint:disable-next-line: max-line-length
+    client_x509_cert_url: 'https://www.googleapis.com/robot/v1/metadata/x509/firebase-adminsdk-xwc47%40tplaboratorio4-a.iam.gserviceaccount.com'
+  }
+
 };
 
 /*
